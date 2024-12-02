b55c0643cc0fc96cb34e6253dd937e2f04f9af0b
abdurezak <abdurazak.awil1@gmail.com>
2024-12-02 09:50:01 -0500

    return emotions with dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,4 +10,9 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+
+    return {**emotions, **{"dominant_emotion": max(emotions, key=emotions.get)}}
